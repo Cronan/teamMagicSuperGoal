f3a00ce56dbb89031f98a4036b1bd5f41e1ee058
Cronyn, Ivan (London) <ivan.cronyn@man.com>
2018-09-12 09:19:53 +0100

    Adds package structure

--- src/teammagicsupergoal/views.py
+++ src/teammagicsupergoal/views.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
+from teammagicsupergoal import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
@@ -9,4 +7,3 @@ def index():
     return render_template('clean_blog_basic.html', heading='Home',
                            title='teamMagicSuperGoal',
                            subheading='The magic super goal page.')
-
