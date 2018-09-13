550aa7ee2b609be5ac3e861892a3ebb5b1c765d8
Tim <tim@localhost.localdomain>
2018-09-13 11:08:04 +0100

    Fix build

--- src/tests/test_timeseries.py
+++ src/tests/test_timeseries.py
@@ -1,4 +1,4 @@
-import timeseries
+import teammagicsupergoal.timeseries as ts
 import datetime
 import numpy as np
 
@@ -9,24 +9,24 @@ for ii in range(1,10) :
 vals = [100.0, 102.0, 99.0, 101.0, 103.0, \
         101.5, 103,0, 104.0, 103.5, 105]
 
-test_ts = timeseries.Timeseries(dts, vals, timeseries.TimeseriesType.PRICE, timeseries.TimeseriesSubType.ABSOLUTE, 1)
+test_ts = ts.Timeseries(dts, vals, ts.TimeseriesType.PRICE, ts.TimeseriesSubType.ABSOLUTE, 1)
 
 def test_returns_absolute():
-    returns_ts = test_ts.calculate_returns(timeseries.TimeseriesSubType.ABSOLUTE, 1)
+    returns_ts = test_ts.calculate_returns(ts.TimeseriesSubType.ABSOLUTE, 1)
     assert len(returns_ts.dates) == len(test_ts.dates - 1)
     for ii in range(9):
         assert np.isclose(returns_ts.values[ii], vals[ii + 1] - vals[ii])
         assert returns_ts.dates[ii] == dts[ii]
 
 def test_returns_fractional():
-    returns_ts = test_ts.calculate_returns(timeseries.TimeseriesSubType.FRACTIONAL, 2)
+    returns_ts = test_ts.calculate_returns(ts.TimeseriesSubType.FRACTIONAL, 2)
     assert len(returns_ts.dates) == len(test_ts.dates - 2)
     for ii in range(8):
         assert np.isclose(returns_ts.values[ii], vals[ii + 2] / vals[ii])
         assert returns_ts.dates[ii] == dts[ii]
 
 def test_returns_log():
-    returns_ts = test_ts.calculate_returns(timeseries.TimeseriesSubType.FRACTIONAL, 3)
+    returns_ts = test_ts.calculate_returns(ts.TimeseriesSubType.FRACTIONAL, 3)
     assert len(returns_ts.dates) == len(test_ts.dates - 3)
     for ii in range(7):
         assert np.isclose(returns_ts.values[ii], np.log( vals[ii + 3] / vals[ii]))
